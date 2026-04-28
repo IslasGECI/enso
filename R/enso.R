0242ef353dbf39bee1bd626cc535156f04ecb8d3
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-04-28 10:37:37 -0700

    ♻️  Refactor

--- R/enso.R
+++ R/enso.R
@@ -1,9 +1,9 @@
 compute_season <- function(date) {
-  mes <- substr(date, 6, 7)
+  month <- substr(date, 6, 7)
   dplyr::case_when(
-    mes %in% c("01", "02", "03") ~ "Winter",
-    mes %in% c("04", "05", "06") ~ "Spring",
-    mes %in% c("07", "08", "09") ~ "Summer",
-    mes %in% c("10", "11", "12") ~ "Fall"
+    month %in% c("01", "02", "03") ~ "Winter",
+    month %in% c("04", "05", "06") ~ "Spring",
+    month %in% c("07", "08", "09") ~ "Summer",
+    month %in% c("10", "11", "12") ~ "Fall"
   )
 }
