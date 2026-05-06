654fd99d1659f00f9638e70fa880fca726eae691
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-05-06 09:13:27 -0700

    🛑🧪 Fail tests

--- tests/testthat/test_enso.R
+++ tests/testthat/test_enso.R
@@ -79,11 +79,10 @@ describe("Get neccessary oni values to calculate oni phase", {
   })
 })
 describe("Classify enso phase given 9 oni values", {
-  
-    niña_values <- c(0.5, 0.1, -0.3, -0.5, -0.5, -0.6, -0.8, -0.8, -0.9)
-    it("classify as niña"{
-         obtained <- xxclassiy_enso(niña_values)
-         expected <- "Niña"
-         expect_equal(obtained, expected)
+  niña_values <- c(0.5, 0.1, -0.3, -0.5, -0.5, -0.6, -0.8, -0.8, -0.9)
+  it("classify as niña", {
+    obtained <- xxclassiy_enso(niña_values)
+    expected <- "Niña"
+    expect_equal(obtained, expected)
   })
-    })
+})
