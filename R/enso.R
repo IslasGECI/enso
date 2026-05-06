f2fcaae993a4078295848d513a1a151dcbbfe70a
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-05-06 09:56:09 -0700

    ✅ Pass tests

--- R/enso.R
+++ R/enso.R
@@ -35,7 +35,9 @@ classify_enso <- function(oni_value) {
 }
 
 xxclassiy_enso <- function(oni_values) {
-  if (sum(oni_values <= -0.5) >= 5) "Niña" else "Neutral"
+  runs <- rle(oni_values <= -0.5)
+  print(runs)
+  if (any(runs$lengths[runs$values] >= 5)) "Niña" else "Neutral"
 }
 
 transform_roni_data_to_longer <- function(roni_data) {
