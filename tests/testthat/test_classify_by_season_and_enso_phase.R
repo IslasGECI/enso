31d5ad5a9ede5eb33283e9e919db512fbdce9611
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-05-08 17:10:42 +0000

    🛑🧪 Fail tests

--- tests/testthat/test_classify_by_season_and_enso_phase.R
+++ tests/testthat/test_classify_by_season_and_enso_phase.R
@@ -3,11 +3,13 @@ describe("Classify by season and enso phase", {
     Date = c("2023-08-20", "2025-01-11", "2024-04-18")
   )
   it("Return the season and enso phase for each date", {
-    obtained <- classify_by_season_and_enso_phase(data_with_date)
+    noaa_data_df <- readr::read_csv("/workdir/tests/data/roni_data.csv", show_col_types = FALSE)
+    obtained <- classify_by_season_and_enso_phase(data_with_date, noaa_data_df)
     expected <- c("Date", "Season", "ENSO_Phase")
     expect_equal(colnames(obtained), expected)
     obtained_seasons <- obtained$Season
     expected_seasons <- c("Summer", "Winter", "Spring")
     expect_equal(obtained_seasons, expected_seasons)
+    expect_equal(obtained$ENSO_Phase, c("Niño", "Niña", "Neutral"))
   })
 })
