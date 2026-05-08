2ed606833f4ba49353bbb00ded4c216b9a55fe42
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-05-08 09:24:35 -0700

    ♻️  Refactor

--- R/enso.R
+++ R/enso.R
@@ -1,13 +1,3 @@
-compute_season <- function(date) {
-  month <- substr(date, 6, 7)
-  dplyr::case_when(
-    month %in% c("01", "02", "03") ~ "Winter",
-    month %in% c("04", "05", "06") ~ "Spring",
-    month %in% c("07", "08", "09") ~ "Summer",
-    month %in% c("10", "11", "12") ~ "Fall"
-  )
-}
-
 compute_enso <- function(date, oni_data) {
   date <- as.Date(date)
   year <- as.numeric(format(date, "%Y"))
