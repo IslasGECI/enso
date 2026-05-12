eda7edcd4238c983990d58d985284e98c061e108
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-05-12 09:12:52 -0700

    🛑🧪 Fail tests

--- tests/testthat/test_enso.R
+++ tests/testthat/test_enso.R
@@ -35,17 +35,17 @@ describe("Return if the date corresponds to an El Niño, La Niña or neutral eve
   noaa_data_df <- readr::read_csv("/workdir/tests/data/roni_data.csv", show_col_types = FALSE)
   it("Return niño for month 8", {
     expected <- "Niño"
-    obtained <- compute_enso("2023-08-01", noaa_data_df)
+    obtained <- xxcompute_enso("2023-08", noaa_data_df)
     expect_equal(expected, obtained)
   })
   it("Return niña for month 1", {
     expected <- "Niña"
-    obtained <- compute_enso("2025-01-01", noaa_data_df)
+    obtained <- xxcompute_enso("2025-01", noaa_data_df)
     expect_equal(expected, obtained)
   })
   it("Return Neutral", {
     expected <- "Neutral"
-    obtained <- compute_enso("2024-04-01", noaa_data_df)
+    obtained <- xxcompute_enso("2024-04", noaa_data_df)
     expect_equal(expected, obtained)
   })
 })
